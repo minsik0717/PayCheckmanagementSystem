864f4544e55b67b5cd80026a3ef63ba7c7fbe975
최민식 <@DESKTOP-HMOS4JG>
2020-03-23 01:39:09 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("*** PayCheck Management System Menu ***");
+		System.out.println(" 1.Producer");
+		System.out.println(" 2.Employee");
+		System.out.println(" 3.Executive");
+		System.out.println(" 4.purchase and expense");
+		System.out.println(" 5.Exit");
+		System.out.println("Select one number between 1 - 5");
+	}
+
+}
